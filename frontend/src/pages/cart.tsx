a95d7d35259026280bcb473eefe63d2383f6e5c9
Sonu <97361485+SoONUPrasad@users.noreply.github.com>
2024-03-09 22:09:40 +0530

    create cart-page

--- frontend/src/pages/cart.tsx
+++ frontend/src/pages/cart.tsx
@@ -1,7 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { VscError } from "react-icons/vsc";
+import CartItem from "../components/CartItem";
+import { Link } from "react-router-dom";
 
-const cartItem = [];
+const cartItem = [
+  {
+    productId: "jkncna",
+    photo:
+      "https://images.unsplash.com/photo-1536627217140-899b0bc9d881?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjJ8fGNhbWVyYXxlbnwwfHwwfHx8MA%3D%3D",
+      productName: "MacBook",
+      productPrice: 4545,
+      productQuantity: 4,
+      productStock: 10,
+  },
+];
 const subTotal = 4000;
 const tax = Math.round(subTotal * 0.18);
 const shippingCharges = 200;
@@ -12,15 +24,31 @@ const Cart = () => {
   const [coupon, setCoupon] = useState<string>("");
   const [isValidCouponCode, setIsValidCouponCode] = useState<boolean>(false);
 
+  useEffect(() => {
+    const timeOutId = setTimeout(() => {
+      if (Math.random() > 0.5) setIsValidCouponCode(true);
+      else setIsValidCouponCode(false);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timeOutId);
+      setIsValidCouponCode(false);
+    };
+  }, [coupon]);
+
   return (
     <div className="cart">
-      <main></main>
+      <main>
+        {cartItem.length > 0 ? cartItem.map((i, idx) => (
+          <CartItem key={idx} cartItem={i}/>
+        )) : <h1>No Items Edit</h1>}
+      </main>
       <aside>
         <p>Subtotal: ${subTotal}</p>
         <p>Tax: ${tax}</p>
         <p>Shipping: ${shippingCharges}</p>
         <p>
-          Discount: <em> - ${discount}</em>
+          Discount: <em className="red"> - ${discount}</em>
         </p>
         <p>
           <b>Total: ${total}</b>
@@ -32,8 +60,8 @@ const Cart = () => {
           onChange={(e) => setCoupon(e.target.value)}
         />
 
-        {coupon && (
-          isValidCouponCode ? (
+        {coupon &&
+          (isValidCouponCode ? (
             <span className="green">
               ${discount} off using the <code>{coupon}</code>
             </span>
@@ -41,8 +69,10 @@ const Cart = () => {
             <span className="red">
               Invalid Coupon <VscError />
             </span>
-          )
-        )}
+          ))}
+
+
+          {cartItem.length > 0 && <Link to={"/shipping"}>Checkout</Link>}
       </aside>
     </div>
   );
